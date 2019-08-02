a49c84b41320a62a47e78684ed41f5117a7c85ad
DebraJohn <debra.zhuang@zeasn.com>
2019-08-02 16:18:22 +0800

    update

---
+++ precache-manifest.cc26c19e57534d2248368a548a59f724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1a4a9d2dd34f86f744b7b553501165",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63c027cc053c082d31cd",
+    "url": "/static/css/main.8650ed4a.chunk.css"
+  },
+  {
+    "revision": "7f987064b266b1f3007f",
+    "url": "/static/js/2.9651d192.chunk.js"
+  },
+  {
+    "revision": "63c027cc053c082d31cd",
+    "url": "/static/js/main.9d469a35.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
