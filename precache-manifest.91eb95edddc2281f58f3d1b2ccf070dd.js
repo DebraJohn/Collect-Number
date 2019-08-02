ba15e8ac8364f8ae0de76ba91b2f235544f0e641
DebraJohn <debra.zhuang@zeasn.com>
2019-08-02 16:23:15 +0800

    update

---
+++ precache-manifest.91eb95edddc2281f58f3d1b2ccf070dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c77ad446ae9eb31d9f12186b1d672e",
+    "url": "/Collect-Number/index.html"
+  },
+  {
+    "revision": "63c027cc053c082d31cd",
+    "url": "/Collect-Number/static/css/main.8650ed4a.chunk.css"
+  },
+  {
+    "revision": "7f987064b266b1f3007f",
+    "url": "/Collect-Number/static/js/2.9651d192.chunk.js"
+  },
+  {
+    "revision": "63c027cc053c082d31cd",
+    "url": "/Collect-Number/static/js/main.9d469a35.chunk.js"
+  },
+  {
+    "revision": "bb9ef4d15814c50a175d",
+    "url": "/Collect-Number/static/js/runtime~main.323d4445.js"
+  }
+]);
